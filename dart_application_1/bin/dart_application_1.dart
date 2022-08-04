eec2ee46fcf05ec6bc209490287f691873a5694c
lucianofaisao <luciano.faisao@gmail.com>
2022-08-04 16:17:39 -0300

    configuracao-projeto

---
+++ dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,16 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/introducao.dart' as introducao;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_1.calculate()}!');
+  introducao.executar();
+  print("digite o seu nome: ");
+  String meuNome = stdin.readLineSync()!;
+
+  print('O nome é: $meuNome');
+
+
+
+
+}
---
+++ dart_application_1/lib/introducao.dart
@@ -0,0 +1,6 @@
+void executar()
+{
+  print('introdução');
+  var nome = 'Luciano';
+  print('O nome é '+nome);
+}
