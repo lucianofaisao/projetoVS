115f0f71e26a0597b2b451d9882d1ba809fcd2d9
lucianofaisao <luciano.faisao@gmail.com>
2022-08-04 16:31:11 -0300

    ALTERACAO DA TELA2

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,6 +1,10 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
 import 'package:dart_application_1/introducao.dart' as introducao;
 import 'dart:io';
+
+/// AULA CONFIGURACAO GITHUB COM HELIO KAMAKAWA
+///
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
@@ -9,8 +13,4 @@ void main(List<String> arguments) {
   String meuNome = stdin.readLineSync()!;
 
   print('O nome é: $meuNome');
-
-
-
-
 }
