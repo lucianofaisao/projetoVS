4c6e2ef6d044f9ee95896fe91f9916439ee263d3
lucianofaisao <luciano.faisao@gmail.com>
2022-08-04 16:39:29 -0300

    teste

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -4,7 +4,7 @@ import 'package:dart_application_1/introducao.dart' as introducao;
 import 'dart:io';
 
 /// AULA CONFIGURACAO GITHUB COM HELIO KAMAKAWA
-///
+/// Configurando o GIT no VSCode. Entenda de vez o que é Git e diferença entre o GitHub
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
